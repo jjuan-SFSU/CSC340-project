c98cfa717b21c1279349d6e72b259cea27bfef42
ilau99 <73664442+ilau99@users.noreply.github.com>
2024-12-11 10:36:16 -0800

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+#include "LinkedList.h"
+
+using namespace std;
+
+struct Menu {
+
+public:
+	void loadMenu(LinkedList& menu, const string& menuChoice);
+
+};
+
+#endif
