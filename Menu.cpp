794bd343cbc97f106f6e9917dd369110a16a2547
ilau99 <73664442+ilau99@users.noreply.github.com>
2024-12-11 10:36:50 -0800

    Create Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,21 @@
+#include "Menu.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void Menu::loadMenu(LinkedList& menu, const string& menuChoice) {
+    ifstream file(menuChoice);
+    string food;
+
+    if (!file.is_open()) {
+        cout << "Error opening file: " << menuChoice << endl;
+        return;
+    }
+
+    while (getline(file, food)) {
+        menu.push_back(food);
+    }
+
+    file.close();
+}
